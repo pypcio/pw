a833b4602bf481e684f0e41b998687b8aa101c0e
Szymon Tymcio <s.tymcio98@gmail.com>
2023-04-29 10:20:22 +0200

    b123

--- registerSw.js
+++ registerSw.js
@@ -1,4 +1,5 @@
 // Handle the push event in the service worker
+const webpush = require("web-push");
 self.addEventListener("push", function (event) {
   console.log("Push event received:", event);
 
@@ -12,6 +13,17 @@ self.addEventListener("push", function (event) {
     data: data.url || "/",
     actions: data.actions || [],
   };
+  let sub = {
+    endpoint:
+      "https://fcm.googleapis.com/fcm/send/dtTXMzHibOA:APA91bF9K-FmcDSXwR2r7itoMI-Zmdt8JTWNFnkrjMcB5cHn45io_yP36xn1cvbj4QQ7kVMSYMB99fdAVcuRCnS3T_qLp91ie5_GiDGdX5QRACu2j4wxq2vJ8gImwah9WiLxLPG3UXqL",
+    expirationTime: null,
+    keys: {
+      p256dh:
+        "BGLAjTnKvwbiH3RxRTGrNhQiDHjyeavuvvUNnKjuQj-pBM_75IVjgk6BFY3zKI9DmiY8UXyaWh5C0MpSZS7qiI4",
+      auth: "xaTwvAPDc6wG7beohCSUOw",
+    },
+  };
+  webpush.sendNotification(sub, "test message");
 
   // Show the notification
   event.waitUntil(self.registration.showNotification(title, options));
